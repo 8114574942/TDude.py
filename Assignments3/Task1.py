b382621fc33d0c18a2d09fc6c542d1f0a1d3fb97
student-myproject <premkumar811457@gmail.com>
2025-06-18 13:27:36 +0530

    Added assignments3

---
+++ Assignments3/Task1.py
@@ -0,0 +1,12 @@
+
+
+
+
+def factorial(n):
+    if n<2:
+        return 2
+    else:
+     return n*(factorial(n-1))
+
+result=factorial(5)    
+print(result)
---
+++ Assignments3/Task2.py
@@ -0,0 +1,5 @@
+import math
+value=float(input("Enter your num :"))
+print("Square root: ",math.sqrt(value))
+print("Logarithm: " ,math.log(value))
+print("Sine: ", math.sin(value))
